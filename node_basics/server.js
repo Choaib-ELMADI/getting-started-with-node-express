0f4608f5dab8dce3ae116a9616c27f7074fad3c0
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-10 15:02:53 +0100

    Sending html pages and rounting

--- node_basics/server.js
+++ node_basics/server.js
@@ -1,14 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-
-    //! set header content type
     // res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<h1>Hello, World</h1>');
-    res.end();
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        case '/blogs':
+            path += 'blogs.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
