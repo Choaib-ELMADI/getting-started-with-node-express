4af4b77a778cce685dbe114c537ca26d2810cd88
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-10 14:17:11 +0100

    Sending back response

--- node_basics/server.js
+++ node_basics/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Request made');
+    console.log(req.url, req.method);
+
+    //! set header content type
+    // res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<h1>Hello, World</h1>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
