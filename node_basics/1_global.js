6349814a1c1e89f1a1ec14bd0cd32208b701aea3
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-10 10:21:16 +0100

    The global object

--- node_basics/1_global.js
+++ node_basics/1_global.js
@@ -1,15 +1,11 @@
-//! The Global Object
+setTimeout(() => {
+    console.log("Good bye.");
+    clearInterval(hello);
+}, 5000);
 
-//* global is the global object in node.js:
-
-// setTimeout(() => {
-//     console.log("Good bye.");
-//     clearInterval(hello);
-// }, 5000);
-
-// const hello = setInterval(() => {
-//     console.log("Hello,");
-// }, 1000);
+const hello = setInterval(() => {
+    console.log("Hello,");
+}, 1000);
 
 console.log(__dirname);
 console.log(__filename);
--- node_basics/test.js
+++ node_basics/test.js
@@ -1,14 +1,1 @@
-// const me = "Choaib";
-// console.log(me);
-
-// const numbers = [2, 3, 4, 7];
-// doubleNumbers = numbers.map(n => n*2);
-// console.log(doubleNumbers);
-
-const people = [ "Hassan", "Hakima", "Salah", "Soufiane" ];
-const ages = [ 30, 40, 26, 13 ];
-
-module.exports = {
-    people,
-    ages
-};
+console.log(global);
