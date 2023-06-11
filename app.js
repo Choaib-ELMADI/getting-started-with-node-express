36a2498f4f2c3bc7ee7fbc4720b57a9fc552933b
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-11 12:56:33 +0100

    Requests

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.redirect('/blogs');
@@ -29,6 +30,23 @@ app.get('/blogs', (req, res) => {
         .catch((err) => {
             console.error(err);
         })
+});
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then(() => res.redirect('/'))
+        .catch((err) => console.error(err));
+});
+
+app.get('/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id)
+        .then((blog) => res.render('blog', { title: 'Blog', blog }))
+        .catch((err) => console.error(err));
+});
+app.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndDelete(req.params.id)
+        .then(() => res.json({ redirect: '/blogs' }))
+        .catch((err) => console.error(err));
 });
 
 app.get('/blogs/new', (req, res) => {
